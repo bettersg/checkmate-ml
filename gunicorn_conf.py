4e2f1c45e77efa8cf45eb85859321c8fdbe0919f
vincentteyssier <v.teyssier@gmail.com>
2023-05-23 19:57:20 +0800

    Changes for server deployment

---
+++ gunicorn_conf.py
@@ -0,0 +1,12 @@
+from multiprocessing import cpu_count
+
+bind = "127.0.0.1:8000"
+
+# Worker Options
+workers = cpu_count() + 1
+worker_class = 'uvicorn.workers.UvicornWorker'
+
+# Logging Options
+loglevel = 'debug'
+accesslog = '/opt/checkmate-ml-models/access_log'
+errorlog =  '/opt/checkmate-ml-models/error_log'
