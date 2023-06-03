9ba694aab5ac367a1a657818104ab5ecc47b0a20
Bing Wen Tan <bingwen.cpf@gmail.com>
2023-06-03 17:35:36 +0800

    added new classifier

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = FastAPI()
 embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 representative_embeddings = np.load("files/representative_embeddings.npy")
 trivial_svc = joblib.load('files/trivial_svc.joblib')
+L1_svc = joblib.load('files/L1_svc.joblib')
 
 class Item(BaseModel):
     text: str
@@ -25,3 +26,9 @@ async def checkTrivial(item: Item):
     similarity_features = cosine_similarity(embedding.reshape(1,-1),representative_embeddings)
     prediction = trivial_svc.predict(similarity_features)[0]
     return {'prediction': "trivial" if prediction == 1 else "non-trivial"}
+
+@app.post("/getL1Category")
+async def checkTrivial(item: Item):
+    embedding = embedding_model.encode(item.text)
+    prediction = L1_svc.predict(embedding.reshape(1,-1))[0]
+    return {'prediction': prediction}
