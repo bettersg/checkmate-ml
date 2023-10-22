df373aa3f961f077db3439b5d99feec96eeae92a
Bing Wen Tan <bingwen.cpf@gmail.com>
2023-10-22 17:55:40 +0800

    updated

--- app.py
+++ app.py
@@ -24,13 +24,6 @@ async def get_embedding(item: ItemText):
   embedding= embedding_model.encode(item.text)
   return {'embedding': embedding.tolist()}
 
-@app.post("/checkTrivial")
-async def checkTrivial(item: ItemText):
-  embedding = embedding_model.encode(item.text)
-  similarity_features = cosine_similarity(embedding.reshape(1,-1),representative_embeddings)
-  prediction = trivial_svc.predict(similarity_features)[0]
-  return {'prediction': "trivial" if prediction == 1 else "non-trivial"}
-
 @app.post("/getL1Category")
 async def getL1Category(item: ItemText):
   embedding = embedding_model.encode(item.text)
@@ -40,9 +33,15 @@ async def getL1Category(item: ItemText):
 @app.post("/ocr")
 async def getOCR(item: ItemUrl):
   output, is_convo, extracted_message, sender = end_to_end(item.url)
+  if extracted_message:
+    embedding = embedding_model.encode(extracted_message)
+    prediction = L1_svc.predict(embedding.reshape(1,-1))[0]
+  else:
+    prediction = "unsure"
   return {
     'output': output,
     'is_convo': is_convo,
     'extracted_message': extracted_message,
     'sender': sender,
+    'prediction': prediction,
   }
